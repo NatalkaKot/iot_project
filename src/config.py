0131368e7754fb5c5a9f94a47d54df97a03c444f
Natalka Kotlarek <natalka.kotlarek@gmail.com>
2022-12-15 12:04:11 +0100

    Dodano klasę do zarządzania konfiguracją

---
+++ src/config.py
@@ -0,0 +1,41 @@
+from configparser import ConfigParser
+
+
+class Config:
+    def __init__(self):
+        self.config = ConfigParser()
+        self.__load_config()
+
+    def __load_config(self):
+        self.config.read('config.ini')
+        to_save = False
+
+        if not self.config.has_section('server'):
+            self.config.add_section('server')
+            to_save = True
+
+        if not self.config.has_option('server', 'url'):
+            url = input('Podaj adres serwera: ')
+            self.config.set('server', 'url', url)
+            to_save = True
+
+        if not self.config.has_section('devices'):
+            self.config.add_section('devices')
+            to_save = True
+
+        if to_save:
+            self.__save_config()
+
+    def __save_config(self):
+        with open('config.ini', 'w') as configfile:
+            self.config.write(configfile)
+
+    def get_server_url(self):
+        return self.config.get('server', 'url')
+
+    def get_device(self, device_name):
+        if not self.config.has_option('devices', device_name):
+            connection_string = input(f'Podaj connection_string urządzenia {device_name}: ')
+            self.config.set('devices', device_name, connection_string)
+            self.__save_config()
+        return self.config.get('devices', device_name)
--- src/main.py
+++ src/main.py
@@ -1,2 +1,14 @@
+from asyncio import get_event_loop
+from asyncua import Client
+from config import Config
+
+
+async def main():
+    config = Config()
+
+    async with Client(config.get_server_url()) as client:
+        await client.check_connection()
+
 if __name__ == '__main__':
-    print('Witaj świecie!')
+    loop = get_event_loop()
+    loop.run_until_complete(main())
